f262bb707ee4ba6f9feb6d73c6668d4f850f1838
swethavasudevaiah123 <132126078+swethavasudevaiah123@users.noreply.github.com>
2023-05-14 01:12:43 +0530

    Update deploy.js

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -7,7 +7,7 @@
 const hre = require("hardhat");
 
 async function main() {
-  const initBalance = 1;
+  const initBalance = 0;
   const Assessment = await hre.ethers.getContractFactory("Assessment");
   const assessment = await Assessment.deploy(initBalance);
   await assessment.deployed();
